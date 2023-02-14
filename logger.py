9cf75aad2544684e0568927270fde10f6eb6f9d4
theraginggoblin <43911061+theraginggoblin@users.noreply.github.com>
2023-02-14 23:48:55 +1100

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,76 @@
+from abc import ABC, abstractmethod
+import threading
+import queue
+import sys
+
+EXIT_MESSAGE = "EXIT"
+
+class LoggerBase(ABC):
+    @abstractmethod
+    def _process_message(message):
+        pass
+
+    @abstractmethod
+    def _process_queue(self):
+        pass
+
+    @abstractmethod
+    def start_logger(self):
+        pass
+
+    @abstractmethod
+    def stop_logger(self):
+        pass
+
+    @abstractmethod
+    def log_message(self, message):
+        pass
+
+class ThreadLogger(LoggerBase):
+    def __init__(self):
+        self._queue = queue.Queue(maxsize=100)
+        self._thread = None
+
+    def _process_message(self, message):
+        print(message)
+
+    def _process_queue(self):
+        while True:
+            message = self._queue.get()
+            if message:
+                if message == EXIT_MESSAGE:
+                    print("Really exiting")
+                    break
+                self._process_message(message)
+
+    def start_logger(self):
+        self._thread = threading.Thread(target=self._process_queue)
+        self._thread.start()
+
+    def stop_logger(self):
+        self._queue.put(EXIT_MESSAGE)
+        self._thread.join()
+
+    def log_message(self, message):
+        self._queue.put(message)
+
+class LoggerFactoryBase(ABC):
+    @staticmethod
+    @abstractmethod
+    def get_logger() -> LoggerBase:
+        pass
+
+class ThreadLoggerFactory(LoggerFactoryBase):
+    @staticmethod
+    def get_logger() -> LoggerBase:
+        return ThreadLogger()
+
+logger_singleton = ThreadLoggerFactory().get_logger()
+logger_singleton.start_logger()
+
+def exeption_handler(exception_type, value, traceback):
+    print("stopping logger in exception handler")
+    logger_singleton.stop_logger()
+    sys.__excepthook__(exception_type, value, traceback)
+
+sys.excepthook = exeption_handler
