99fb5895db0d008da5f80599bf89011e2ffe1beb
theraginggoblin <43911061+theraginggoblin@users.noreply.github.com>
2023-02-14 23:48:37 +1100

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from logger import logger_singleton
+from time import sleep
+
+if __name__ == "__main__":
+    print("Logging 20 messages")
+    for num in range(0, 20):
+        logger_singleton.log_message(f"{str(num)}")
+
+    exception_test = False
+
+    if exception_test:
+        try:
+            raise Exception
+        except Exception as e:
+            print("caugth exception")
+
+        raise Exception
+
+    print("Sleeping for 3 seconds")
+    sleep(3)
+
+    print("Stopping logger in main")
+    logger_singleton.stop_logger()
